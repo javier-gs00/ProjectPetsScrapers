8e8ad6c64b7516d17c2c8632720a4996460dd5f2
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-08-24 23:54:42 -0300

    Added express and handlebars-express modules

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 // const meddaymascotas = require('./scrapers/meddaymascotas.js');
-const noi = require('./scrapers/noi.js')
+// const noi = require('./scrapers/noi.js')
+const express = require('express')
+const exphbs = require('express-handlebars')
 const fs = require('fs')
 const mongoose = require('mongoose')
-const savecollection = require('./savecollection.js')
+// const savecollection = require('./collections/savecollection.js')
 
-mongoose.connect('mongodb://localhost/projectpets')
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+const app = express()
+const router = express.Router()
+const path = __dirname + '/'
+// mongoose.connect('mongodb://localhost/projectpets')
+// const db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error:'))
 
-savecollection.save('medsnoi.json')
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+// savecollection.save('medsnoi.json')
 
 // let date = new Date()
 // console.log('Process started at: ' + date.getSeconds())
 // meddaymascotas.scrapper();
 // noi.scraper()
 
-console.log('end of process')
+app.listen(3000, function () {
+    console.log('App listening on port 3000')
+})
