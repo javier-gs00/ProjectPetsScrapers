66f16e7c9927978d53910a4b517af66783d8b6a9
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-09-29 20:03:26 -0300

    Session stores implemented with the connect-mongodb-sessions module

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 const passport = require('passport')
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/projectpets')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+const store = new MongoDBStore (
+    {
+        uri: 'mongodb://localhost/projectpets',
+        collection: 'Sessions'
+    }
+)
+
+// Catch Errors
+store.on('error', function (error) {
+    assert.ifError(error)
+    assert.ok(false)
+})
+
 app.use(bodyParser.urlencoded({extended: true}))
 // app.use(methodOverride('_method'))
 app.use(methodOverride(function (req, res) {
@@ -19,6 +38,7 @@ app.use(methodOverride(function (req, res) {
 }))
 app.use(session({
     secret: 'secret',
+    store: store,
     resave: false,
     saveUninitialized: false
 }))
@@ -26,10 +46,6 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-
-mongoose.connect('mongodb://localhost/projectpets')
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
 
 app.use('/', routes)
 
